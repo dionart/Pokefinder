bd7754884dac752ad46b82905990dddf5f3cc17d
arthur.dionizio <arthuroliveira@eng.ci.ufpb.br>
2020-12-17 05:59:57 -0300

    pokemon div and floating button added

--- src/components/Search Page/index.tsx
+++ src/components/Search Page/index.tsx
@@ -14,17 +14,6 @@ import axios from 'axios';
 
 //interfaces
 
-interface Neighborhoods {
-  zone: number;
-  neighborhood: any;
-}
-
-interface Neighborhood {
-  id: number;
-  name: string;
-  location: [number, number];
-}
-
 interface User {
   id: string;
   token: string;
@@ -37,19 +26,12 @@ const SearchPage: React.FC = () => {
   //selector para pegar informações do usuário logado
   const user: User = useSelector((state: RootStateOrAny) => state.user.user);
   //declarações de constantes
-  const [ModalChoice, setModalChoice] = useState(false);
   const [showMap, setShowMap] = useState(false);
-  const [selectedPokemon, setSelectedPokemon] = useState<any>();
   const [pokemon, setPokemon] = useState<Pokemon>();
   const [searchText, setSearchText] = useState('');
-  const [zone, setZone] = useState(0);
-  const [neighborhoodId, setNeighborhoodId] = useState(0);
-  const [choice, setChoice] = useState<string>('');
-  const [modalLogin, setModalLogin] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
-    //$("#trump").css("transition", "border 0.1s linear");
     axios.get('https://pokeapi.co/api/v2/pokemon').then(response => {
       console.log(response.data.results);
     });
@@ -57,38 +39,41 @@ const SearchPage: React.FC = () => {
 
   const search = async () => {
     const formattedText = searchText.toLowerCase().trim();
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${formattedText}`,
-    );
-
-    if (response.data.id !== '') {
-      console.log('setei');
-      setPokemon({
-        id: response.data.id !== undefined ? response.data.id : '',
-        name: response.data.name !== undefined ? response.data.name : '',
-        height: response.data.height !== undefined ? response.data.height : '',
-        weight: response.data.weight !== undefined ? response.data.weight : '',
-        sprite:
-          response.data.sprites.front_default !== undefined
-            ? response.data.sprites.front_default
-            : '',
-        type: response.data.types !== undefined ? response.data.types : '',
+    await axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${formattedText}`)
+      .then(response => {
+        if (response.data.id !== '') {
+          console.log('setei');
+          setPokemon({
+            id: response.data.id !== undefined ? response.data.id : '',
+            name: response.data.name !== undefined ? response.data.name : '',
+            height:
+              response.data.height !== undefined ? response.data.height : '',
+            weight:
+              response.data.weight !== undefined ? response.data.weight : '',
+            sprite:
+              response.data.sprites.front_default !== undefined
+                ? response.data.sprites.front_default
+                : '',
+            type: response.data.types !== undefined ? response.data.types : '',
+          });
+        }
+      })
+      .catch(() => {
+        setPokemon(undefined);
       });
-    }
+
     console.log(pokemon);
     //setstuff();
   };
 
-  //handleClose para modais
-  const handleClose = () => {
-    setModalChoice(false);
-  };
-  const handleLogin = () => {
-    setModalLogin(false);
-  };
-
   return (
     <div id="background">
+      <Button className="button-float" variant="danger">
+        <img className="logo" src={logo} />
+        MY POKEDEX
+      </Button>
+
       <div className={showMap ? 'search active' : 'search'}>
         <img className="logo" src={logo} />
         <a>Pokefinder</a>
@@ -124,22 +109,31 @@ const SearchPage: React.FC = () => {
       {pokemon && (
         <div
           className="d-flex"
-          style={{ justifyContent: 'center', alignItems: 'center' }}
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
         >
           <div className="pokemon-container">
-            <img src={pokemon?.sprite} />
-
-            <Row style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <div className="sprite-container">
+              <img src={pokemon?.sprite} />
+            </div>
+            <Row className="row-id">
               <div>
                 <img
-                  src="https://icon-library.com/images/pokeball-icon-png/pokeball-icon-png-12.jpg"
-                  style={{ width: '20px', height: '20px' }}
+                  src="https://www.pngkit.com/png/full/19-190666_pokeball-graphic-by-maratuna-on-deviantart-banner-free.png"
+                  style={{
+                    width: '15px',
+                    height: '15px',
+                    marginTop: '-2px',
+                    padding: '2px',
+                  }}
                 />
                 <span>{pokemon.id}</span>
               </div>
-              <h3>{pokemon.name}</h3>
-            </Row>
 
+              <h3>{pokemon.name.toLocaleUpperCase()}</h3>
+            </Row>
             <Row style={{ height: '30px', justifyContent: 'center' }}>
               {pokemon.type.map(typeIndex => (
                 <div className={`${typeIndex.type.name}`}>
@@ -147,6 +141,9 @@ const SearchPage: React.FC = () => {
                 </div>
               ))}
             </Row>
+            <Button className="button-boot" variant="danger">
+              ADD TO MY POKEDEX
+            </Button>{' '}
           </div>
         </div>
       )}
